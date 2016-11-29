4fe945444a37213dbc6e3a1502d6ab48ab6b0def
Jonah Elias Nitschke <lejonah@web.de>
2016-11-29 18:47:22 +0100

    Started Plot

---
+++ Protokolle/V201_Das_Dulong-Petitsche_Gesetz/Auswertung/Temperatur.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.stats import sem
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+U = np.array([0.0, 3.88])
+T = np.array([0.0, 100.0])
+
+def f(x, A, B):
+    return A * x + B
+
+params, covariance = curve_fit(f, U, T)
+
+x_plot = np.linspace(0, 4, 1000)
+
+plt.plot(U, T, "rx", label = "Spannungen")
+plt.plot(x_plot, f(x_plot, *params), "b-")
+#plt.show()
+
+print(params[0] * 0.82 + params[1])
