9ba06e11ce96755da737c14fa04c735abe90d26e
Jonah Elias Nitschke <lejonah@web.de>
2017-04-11 22:37:33 +0200

    Durchführung und Auswertung eigentlich vervollständgt, werde an meinem Teil aber noch ein bisschen arbeiten

--- Zusatzversuch_Wirbelstrombremse/Plots.py
+++ Zusatzversuch_Wirbelstrombremse/Plots.py
@@ -7,9 +7,17 @@ import uncertainties.unumpy as unp
 
 Stromstärken = np.array([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
 
-Messung_1 = np.array([237, 93, 37, 25,14, 10, 7, 5, 4, 3, 2])
+Messung_1 = np.array([237, 93, 37, 25, 14, 10, 7, 5, 4, 3, 2])
 Messung_2 = np.array([256, 179, 125, 79, 58, 41, 38, 31, 21, 18, 14])
 Messung_3 = np.array([272, 244, 210, 165, 124, 98, 84, 70, 60, 50, 44])
+
+Dämpfung1 = ufloat(Messung_1[10], 0.5) / ufloat(Messung_1[0], 0.5)
+Dämpfung2 = ufloat(Messung_2[10], 0.5) / ufloat(Messung_2[0], 0.5)
+Dämpfung3 = ufloat(Messung_3[10], 0.5) / ufloat(Messung_3[0], 0.5)
+
+print("Die Dämpfung bei der ungeschlitzten Aluminiumplatte beträgt ca. ", Dämpfung1*100, "%.", '\n')
+print("Die Dämpfung bei der mittel geschlitzten Aluminiumplatte beträgt ca. ", Dämpfung2*100, "%.", '\n')
+print("Die Dämpfung bei der maximal geschlitzten Aluminiumplatte beträgt ca. ", Dämpfung3*100, "%.", '\n')
 
 #Messung 1
 
@@ -20,6 +28,7 @@ plt.xlim(-0.5, 5.5)
 plt.xlabel(r'Stromstärke in A')
 plt.ylabel(r'Zeit in s')
 plt.legend(loc = 'best')
+plt.savefig('Messung1.pdf')
 #plt.show()
 
 #Messung 2
@@ -31,6 +40,7 @@ plt.xlim(-0.5, 5.5)
 plt.xlabel(r'Stromstärke in A')
 plt.ylabel(r'Zeit in s')
 plt.legend(loc = 'best')
+plt.savefig('Messung2.pdf')
 #plt.show()
 
 #Messung 3
@@ -42,6 +52,7 @@ plt.xlim(-0.5, 5.5)
 plt.xlabel(r'Stromstärke in A')
 plt.ylabel(r'Zeit in s')
 plt.legend(loc = 'best')
+plt.savefig('Messung3.pdf')
 #plt.show()
 
 #Übereinander
@@ -55,4 +66,5 @@ plt.xlim(-0.5, 5.5)
 plt.xlabel(r'Stromstärke in A')
 plt.ylabel(r'Zeit in s')
 plt.legend(loc = 'best')
-plt.show()
+plt.savefig('Kombiniert.pdf')
+#plt.show()
