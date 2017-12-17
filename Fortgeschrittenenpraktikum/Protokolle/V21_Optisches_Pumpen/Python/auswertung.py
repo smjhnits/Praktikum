1b7dada6bf77ccbd6312d1a267e713b2ab04f1ba
Sebastian Pape <SePa@gmx.de>
2017-12-17 23:26:26 +0100

    weitergearbeitet

--- Fortgeschrittenenpraktikum/Protokolle/V21_Optisches_Pumpen/Python/auswertung.py
+++ Fortgeschrittenenpraktikum/Protokolle/V21_Optisches_Pumpen/Python/auswertung.py
@@ -15,15 +15,19 @@ Q_ = u.Quantity
 
 mu_bohr = Q_(const.physical_constants['Bohr magneton'][0], 'joule/tesla')
 e_0 = Q_(const.e, 'coulomb')
+e_0_dimlos = const.e
+m_0 = Q_(const.m_e, 'kg')
+m_0_dimlos = const.m_e
+mu_0 = Q_(const.mu_0, 'N / A**2')
 
 ### Angaben aus Protokoll ###
 
 N_sweep = 11
 N_horizontal = 154
 N_vertical = 20
-R_sweep = Q_(16.39, 'cm')
-R_horizontal = Q_(15.79, 'cm')
-R_vertical = Q_(11.735, 'cm')
+R_sweep = Q_(16.39, 'cm').to('m')
+R_horizontal = Q_(15.79, 'cm').to('m')
+R_vertical = Q_(11.735, 'cm').to('m')
 
 ### Quantenzahlen ###
 
@@ -36,6 +40,7 @@ I_vertical = Q_(2.28 * 0.1, 'A')
 
 ### Messdaten aus Messprogramm c.) ###
 
+frequenz_dimlos = np.array([101, 210, 300, 400, 502, 600, 700, 800, 904, 1001])
 frequenz = Q_(np.array([101, 210, 300, 400, 502, 600, 700, 800, 904, 1001]), 'kHz')
 
 I_sweep_1 = Q_(np.array([5.4, 3.75, 5.53, 4.71, 1.13, 1.77, 0.92, 3.55, 3.97, 5.17]) * 0.1, 'A')
@@ -48,39 +53,52 @@ I_ten_horizontal_2 = Q_(np.array([164, 188, 216]), 'mA')
 ### Magnetfelder aus Spulenstrom berechnen ###
 
 def B_Helmholtz(I, N, R):
-    return mu_bohr * 8 / (np.sqrt(125) * R) * I * N
+    return mu_0 * 8 * I * N / (np.sqrt(125) * R)
 
 
 ### ERDMAGNETFELD Vertikalfeld ###
 
-erdmag_vertical = B_Helmholtz(I_vertical, N_vertical, R_vertical)
+erdmag_vertical = B_Helmholtz(I_vertical, N_vertical, R_vertical).to('tesla')
 
 print('Erdmagnetfeld: ', erdmag_vertical)
 
 ### B-Feld sweep, horizontal ###
 
-B_sweep_1 = B_Helmholtz(I_sweep_1, N_sweep, R_sweep)
-B_sweep_2 = B_Helmholtz(I_sweep_2, N_sweep, R_sweep)
+B_sweep_1 = B_Helmholtz(I_sweep_1, N_sweep, R_sweep).to('tesla')
+B_sweep_2 = B_Helmholtz(I_sweep_2, N_sweep, R_sweep).to('tesla')
 
-B_seven_horizontal = B_Helmholtz(I_seven_horizontal, N_horizontal, R_horizontal)
-B_ten_horizontal_1 = B_Helmholtz(I_ten_horizontal_1, N_horizontal, R_horizontal)
-B_ten_horizontal_2 = B_Helmholtz(I_ten_horizontal_2, N_horizontal, R_horizontal)
+B_seven_horizontal = B_Helmholtz(I_seven_horizontal, N_horizontal, R_horizontal).to('tesla')
+B_ten_horizontal_1 = B_Helmholtz(I_ten_horizontal_1, N_horizontal, R_horizontal).to('tesla')
+B_ten_horizontal_2 = B_Helmholtz(I_ten_horizontal_2, N_horizontal, R_horizontal).to('tesla')
 
 ### g_F bestimmen. g_F = a in poly ###
 
 def poly (x, a, b):
-    return 4 * np.pi * e_0 * 1 / a * x + b
+    return 4 * np.pi * m_0 * 1 /(a * e_0) * x + b
 
-params_lande_sweep_1, covariance_B_sweep_1 = curve_fit(poly, frequenz, B_sweep_1)
-params_lande_sweep_2, covariance_B_sweep_2 = curve_fit(poly, frequenz, B_sweep_2)
+### Sweep Lande sind die, die zu den Übergängen gehören
+params_lande_sweep_1, covariance_B_sweep_1 = curve_fit(poly, B_sweep_1, frequenz_dimlos)
+params_lande_sweep_2, covariance_B_sweep_2 = curve_fit(poly, B_sweep_2, frequenz_dimlos)
 
-params_lande_seven_horizontal, covariance_B_seven_horizontal = curve_fit(poly, frequenz, B_seven_horizontal)
-params_lande_ten_horizontal_1, covariance_B_ten_horizontal_1 = curve_fit(poly, frequenz, B_ten_horizontal_1)
-params_lande_ten_horizontal_2, covariance_B_ten_horizontal_2 = curve_fit(poly, frequenz, B_ten_horizontal_2)
+params_lande_seven_horizontal, covariance_B_seven_horizontal = curve_fit(poly, B_seven_horizontal, frequenz_dimlos[0: 6])
+params_lande_ten_horizontal_1, covariance_B_ten_horizontal_1 = curve_fit(poly, B_ten_horizontal_1, frequenz_dimlos[6: 9])
+params_lande_ten_horizontal_2, covariance_B_ten_horizontal_2 = curve_fit(poly, B_ten_horizontal_2, frequenz_dimlos[6: 9])
 
+print(len(frequenz_dimlos[8: 9]), len(B_ten_horizontal_1))
+#plt.clf()
+#plt.plot(frequenz_dimlos, poly(frequenz_dimlos, *params_lande_sweep_1), "gx", label=r"")
+##plt.plot(frequenz_dimlos, 4 * np.pi * m_0_dimlos * 1 / (e_0_dimlos) * B_sweep_1, "kx", label=r"")
+#plt.xlabel('Frequenz in kHz')
+#plt.ylabel('Magnetfeldstärke in mT')
+#plt.legend(loc='best')
+#plt.tight_layout()
+#plt.show()
 
-### Ermitteln des Kernspins ###
+### Ermitteln des Kernspins ### ?????????
 
-g_J = (3.0023 * J * (J + 1) + 1.0023(S * (S + 1) - L * (L + 1))) / (2 * J * (J * 1))
-  
-Kernspin =
+g_J = (3.0023 * J * (J + 1) + 1.0023 * (S * (S + 1) - L * (L + 1))) / (2 * J * (J * 1))
+
+Kernspin_1 = 1 / 2 * (g_J / params_lande_sweep_1[0] - 1)
+Kernspin_2 = 1 / 2 * (g_J / params_lande_sweep_2[0] - 1)
+
+print(Kernspin_1, Kernspin_2)
