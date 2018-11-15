b47a12eed1a582c5ed58bef7befeb2b53319c790
Jonah Nitschke <lejonah@web.de>
2018-11-15 09:44:15 +0100

    Started working on auswertung.tex2

--- Fortgeschrittenenpraktikum/Protokolle/V49_gepulste_NMR/Python/auswertung.py
+++ Fortgeschrittenenpraktikum/Protokolle/V49_gepulste_NMR/Python/auswertung.py
@@ -131,7 +131,7 @@ plt.legend(loc = 'best')
 plt.ylabel(r'$U$ in $V$')
 plt.xlabel(r'$log(t)$ in $s$')
 plt.xscale('log')
-plt.savefig('../Plots/T1.pdf')
+plt.savefig('../Plots2/T1.pdf')
 #plt.show()
 
 # Plot T2
@@ -144,7 +144,7 @@ plt.plot(t2, FitT2(t2, paramsT2[0], paramsT2[1], paramsT2[2]), 'g-', label = 'Fi
 plt.legend(loc = 'best')
 plt.ylabel(r'$U$ in $V$')
 plt.xlabel(r'$t$ in $s$')
-plt.savefig('../Plots/T2.pdf')
+plt.savefig('../Plots2/T2.pdf')
 #plt.show()
 
 # Log Plot T2
@@ -155,7 +155,7 @@ plt.plot(t2, FitT2(t2, paramsT2[0], paramsT2[1], paramsT2[2]), 'g-', label = 'Fi
 plt.yscale('log')
 plt.xlabel(r'$t$ in $s$')
 plt.ylabel(r'$log(U)$ in $V$')
-plt.savefig('../Plots/T2Log.pdf')
+plt.savefig('../Plots2/T2Log.pdf')
 #plt.show()
 
 # Plot D
@@ -166,5 +166,5 @@ plt.plot(D_tau, D_U, 'rx', label = 'Messwerte')
 plt.plot(td, FitD(td, paramsD[0], paramsD[1], paramsD[2], paramsD[3]), 'g-', label = 'Fit')
 plt.ylabel(r'$U$ in $mV$')
 plt.xlabel(r'$t$ in  $ms$')
-plt.savefig('../Plots/TD.pdf')
+plt.savefig('../Plots2/TD.pdf')
 #plt.show()
