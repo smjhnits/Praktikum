36da3d91afe92ab546486235715969d8b7b2a221
Sebastian-Pape <SePa@gmx.de>
2019-01-14 11:51:18 +0100

    added setup figures

--- Fortgeschrittenenpraktikum/Protokolle/V60_Diodenlaser/Python/auswertung.py
+++ Fortgeschrittenenpraktikum/Protokolle/V60_Diodenlaser/Python/auswertung.py
@@ -20,10 +20,11 @@ df_1 = pd.read_csv('scope_0.csv', skiprows=2, dtype=np.float64, names=['msecond'
 
 x_1 = np.array(range(len(df_1.diode)))
 verschiebung_1 = 0 # default war 700
+verschiebung = 700
 
 plt.clf()
-plt.plot(x_1, df_1.diode, label='PD')
-plt.xlim(0 + verschiebung_1, 1800)
+plt.plot(x_1 - verschiebung, df_1.diode, label='PD')
+plt.xlim(0 + verschiebung_1, 1800 - verschiebung)
 plt.ylim(1.4, 4.5)
 plt.text(220 + verschiebung_1, 2.5, '87b', fontsize=12)
 plt.text(360 + verschiebung_1, 1.5, '85b', fontsize=12)
@@ -40,10 +41,11 @@ df_2 = pd.read_csv('scope_1.csv', skiprows=2, dtype=np.float64, names=['msecond'
 
 x_2 = np.array(range(len(df_2.diode)))
 verschiebung_2 = 0 # default war 650
+verschiebung -= 50
 
 plt.clf()
-plt.plot(x_2, -df_2.diode, label='PD')
-plt.xlim(0 + verschiebung_2, 1700)
+plt.plot(x_2 - verschiebung, -df_2.diode, label='PD')
+plt.xlim(0 + verschiebung_2, 1700 - verschiebung)
 plt.text(120 + verschiebung_2, -0.37, '87b', fontsize=12)
 plt.text(260 + verschiebung_2, -0.64, '85b', fontsize=12)
 plt.text(590 + verschiebung_2, -0.18, '85a', fontsize=12)
